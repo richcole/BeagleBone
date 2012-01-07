849c7174c40efbc02cbf167da535292c460eefd7
Richard Cole <richard.j.cole@gmail.com>
2012-01-07 15:32:33 -0800

    Clean up

--- boot.asm
+++ boot.asm
@@ -1,9 +1,3 @@
-# UART base locations from the TRM
-.equ UART0.BASE,        0x44e09000        
-.equ UART3.BASE, 		0x481a6000
-
- 
-# We need to be in ARM mode - we may branch to Thumb mode later if desired.
 .arm
 _start:
    ldr r0,=0x44e09000
